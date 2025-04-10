8a5ef0e123bd647285b557e71cbf9494d60d5c86
Tushar Verma <231210117@nitdelhi.ac.in>
2025-04-10 23:22:50 +0530

    12345678901234567

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -11,10 +11,33 @@ export async function POST(request){
 
         await connectToDatabase();
         console.log("Connected to database");
-        
+
+        const user = await User.findOne({signUpEmail: email , password: password});
+        console.log("User:", user);
+        if(!user){
+            return new Response("Invalid email or password", {status: 401});
+        }
+        const {accessToken, refreshToken, gmailEmail } = user;
+
+        console.log("Access Token:", accessToken);
+        console.log("Refresh Token:", refreshToken);
+        console.log("Gmail Email:", gmailEmail);
+
+        return new Response(
+            JSON.stringify({
+              message: "Login successful",
+              access_token,
+              refresh_token,
+              gmail_email: signUpEmail,
+            }),
+            { status: 200, headers: { "Content-Type": "application/json" } }
+          );
+
 
     }catch(error){
         console.error("Error in login route:", error);
-        return new Response("Error in login route", { status: 500 });
+        return new Response(JSON.stringify({
+            message: "Internal server error"
+        }), { status: 500 });
     }
 }
